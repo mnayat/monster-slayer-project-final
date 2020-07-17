6376a127fe5378dcc9af17b4bbda3aae9c3673bf
MarkPianMartinez <59362262+MarkPianMartinez@users.noreply.github.com>
2020-07-17 19:16:03 +0800

    Pm/battle (#63)
    * fixed
    
    * Removed Icons
    Fixed bug in Battle
    
    Co-authored-by: Pian Martinez <PianM@magenic.com>

--- src/components/skills/Skill-Item.vue
+++ src/components/skills/Skill-Item.vue
@@ -14,7 +14,7 @@
                   class="btn btn-primary btn-sm btn-block"
                   @click.prevent="getSkillsDescription(item._id)"
                 >
-                <img :src="require('../../assets/icons/skills/' + item.type + '.png')" />
+                  <!-- <img :src="require('../../assets/icons/skills/' + item.type + '.png')" /> -->
                   {{ item.name }}
                 </button>
               </div>
